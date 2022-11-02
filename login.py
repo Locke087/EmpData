9c3e88a47835ae1c2877b60e49db2ed3ad56571d
kode <kode.creer@gmail.com>
2022-11-02 17:36:04 -0600

    needs to look nicer

--- login.py
+++ login.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, ttk
 import csv
 from hashlib import sha256
 def login():
@@ -29,21 +29,31 @@ def login():
     messagebox.showerror("Invalid credentials", "The username or the password you have entered is not correct")
 # Use Tkinter to create window
 window = tk.Tk()
-# In center of screen, create welcome message, username and password input boxes with username and password headings
-welcome_message = tk.Label(window, text="Welcome", font=('Arial', 25))
-welcome_message.pack()
-# Take in both username and password as input from the user
-username_message = tk.Label(window, text="User Name")
-username_message.pack()
-username = tk.Entry()
-username.pack()
+frame = ttk.Frame(window)
 
-pass_message = tk.Label(window, text="Password")
-pass_message.pack()
+# In center of screen, create welcome message, username and password input boxes with username and password headings
+welcome_message = tk.Label(frame, text="Welcome", font=('Arial', 25), width=50)
+
+# Take in both username and password as input from the user
+username_message = tk.Label(frame, text="User Name")
+
+username = tk.Entry(frame)
+
+
+pass_message = tk.Label(frame, text="Password")
+
 password = tk.StringVar()#Password is stored here
-pass_entry = tk.Entry(window, textvariable=password, show="*")
+pass_entry = tk.Entry(master=frame, textvariable=password, show="*")
+
+submit = tk.Button(frame, text="Submit", command=login)
+
+
+welcome_message.pack()
+username_message.pack()
+username.pack()
+pass_message.pack()
 pass_entry.pack()
-submit = tk.Button(window, text="Submit", command=login)
 submit.pack()
 
+frame.pack()
 window.mainloop()
